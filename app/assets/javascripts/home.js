456f176d18fc8f372607ff599a1664d771fac3c5
Luís Dias <lcdias127@gmail.com>
2016-10-06 21:35:24 +0100

    Adding places search boxes

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -14,7 +14,7 @@ document.addEventListener("turbolinks:load", function() {
 
 function startGMaps(){
   var defaultCity = "porto";
-  async('maps.googleapis.com/maps/api/js?key=AIzaSyBSuC-_fpdtL7xCl1jWperw8d71Ce9jIeU', function() {
+  async('maps.googleapis.com/maps/api/js?key=AIzaSyBSuC-_fpdtL7xCl1jWperw8d71Ce9jIeU&libraries=places', function() {
     showTab(defaultCity);
   });
 }
@@ -28,5 +28,69 @@ function showTab(city){
     mapTypeId: google.maps.MapTypeId.ROADMAP,
     mapTypeControl: false,
     streetViewControl: false
+  });
+  loadPlacesSearch(map);
+}
+
+function loadPlacesSearch(map){
+  // Create the search box and link it to the UI element.
+  var fromInput = document.getElementById('place-from-input');
+  var toInput   = document.getElementById('place-to-input');
+  var searchBox = new google.maps.places.SearchBox(fromInput);
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(fromInput);
+  map.controls[google.maps.ControlPosition.LEFT_TOP].push(toInput);
+
+  // Bias the SearchBox results towards current map's viewport.
+  map.addListener('bounds_changed', function() {
+    searchBox.setBounds(map.getBounds());
+  });
+
+  var markers = [];
+  // Listen for the event fired when the user selects a prediction and retrieve
+  // more details for that place.
+  searchBox.addListener('places_changed', function() {
+    var places = searchBox.getPlaces();
+
+    if (places.length == 0) {
+      return;
+    }
+
+    // Clear out the old markers.
+    markers.forEach(function(marker) {
+      marker.setMap(null);
+    });
+    markers = [];
+
+    // For each place, get the icon, name and location.
+    var bounds = new google.maps.LatLngBounds();
+    places.forEach(function(place) {
+      if (!place.geometry) {
+        console.log("Returned place contains no geometry");
+        return;
+      }
+      var icon = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      };
+
+      // Create a marker for each place.
+      markers.push(new google.maps.Marker({
+        map: map,
+        icon: icon,
+        title: place.name,
+        position: place.geometry.location
+      }));
+
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
+    map.fitBounds(bounds);
   });
 }
