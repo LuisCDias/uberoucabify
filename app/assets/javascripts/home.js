b5d9c5bbaee32ff4c48af6cb88bcb8a959c20a43
Luís Dias <lcdias127@gmail.com>
2016-10-10 10:39:07 +0100

    Finding directions between the two points and rendering path

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,6 +1,9 @@
 var LOCATIONS = { 'porto': {lat: 41.165432, lng: -8.629065}, 'lisbon': {lat: 38.728876, lng: -9.138234} };
 var CUSTOM_MAP_STYLES = [{"featureType":"water","elementType":"geometry","stylers":[{"color":"#e9e9e9"},{"lightness":17}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffffff"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#ffffff"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":16}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":21}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#dedede"},{"lightness":21}]},{"elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#ffffff"},{"lightness":16}]},{"elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#333333"},{"lightness":40}]},{"elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#f2f2f2"},{"lightness":19}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#fefefe"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#fefefe"},{"lightness":17},{"weight":1.2}]}];
 var markers = {};
+var directionsDisplay;
+var directionsService;
+var map;
 
 document.addEventListener("turbolinks:load", function() {
   var phonePictureIntervalId = setInterval(togglePhoneImage, 1500);
@@ -31,6 +34,7 @@ document.addEventListener("turbolinks:load", function() {
       fromInput.value == "" ? fromInput.focus() : toInput.focus();
       return false;
     }
+    calcRoute(fromInput.value,toInput.value)
   })
 });
 
@@ -42,38 +46,37 @@ function startGMaps(){
   var defaultCity = "porto";
   async('maps.googleapis.com/maps/api/js?key=AIzaSyBSuC-_fpdtL7xCl1jWperw8d71Ce9jIeU&libraries=places', function() {
     showTab(defaultCity);
+    directionsService = new google.maps.DirectionsService();
+    directionsDisplay = new google.maps.DirectionsRenderer({preserveViewport: true});
+    directionsDisplay.setMap(map);
   });
 }
 
 function showTab(city){
   $('#'+city+'-map').html("");
-  var map = new google.maps.Map(document.getElementById(city+'-map'), {
+  map = new google.maps.Map(document.getElementById(city+'-map'), {
     center: LOCATIONS[city],
-    zoom: 12,
+    zoom: 13,
     styles: CUSTOM_MAP_STYLES,
     mapTypeId: google.maps.MapTypeId.ROADMAP,
     mapTypeControl: false,
     streetViewControl: false,
-    scrollwheel: false
+    scrollwheel: false,
+    minZoom: 13,
+    maxZoom: 17,
+    panControl: true
   });
-  loadPlacesSearch(map);
-  // var zoomChangeBoundsListener =
-  // google.maps.event.addListener(map, 'bounds_changed', function(event) {
-  //   alert("working");
-  //     google.maps.event.removeListener(zoomChangeBoundsListener);
-  //     map.setZoom( Math.min( 15, map.getZoom() ) );
-  // });
+  loadPlacesSearch();
 }
 
-function loadPlacesSearch(map){
+function loadPlacesSearch(){
   // Create the search box and link it to the UI element.
   var fromInput = document.getElementById('place-from-input');
   var toInput   = document.getElementById('place-to-input');
   var calculateButton = document.getElementById('calculate-cost-button');
   var fromSearchBox = new google.maps.places.SearchBox(fromInput);
   var toSearchBox = new google.maps.places.SearchBox(toInput);
-  var fromIconPath = fromInput.dataset.iconPath;
-  var toIconPath = toInput.dataset.iconPath;
+
   // TODO do something with toSearchBox
   map.controls[google.maps.ControlPosition.TOP_LEFT].push(fromInput);
   map.controls[google.maps.ControlPosition.LEFT_TOP].push(toInput);
@@ -93,20 +96,20 @@ function loadPlacesSearch(map){
     if (places.length == 0) return;
 
     // For each place, get the icon, name and location.
-    place = places[0]
-    setMarkerToPlace(place,map,'from');
+    place = places[0];
+    setMarkerToPlace(place,'from');
   });
   toSearchBox.addListener('places_changed', function() {
     var places = toSearchBox.getPlaces();
     if (places.length == 0) return;
 
     // For each place, get the icon, name and location.
-    place = places[0]
-    setMarkerToPlace(place,map,'to');
+    place = places[0];
+    setMarkerToPlace(place,'to');
   });
 }
 
-function setMarkerToPlace(place,map,markerKey){
+function setMarkerToPlace(place,markerKey){
   if (!place.geometry) {
     console.log("Returned place contains no geometry");
     return;
@@ -126,7 +129,6 @@ function setMarkerToPlace(place,map,markerKey){
   // Create a marker for each place.
   markers[markerKey] = new google.maps.Marker({
     map: map,
-    icon: icon,
     title: place.name,
     position: place.geometry.location
   });
@@ -138,12 +140,30 @@ function setMarkerToPlace(place,map,markerKey){
   }
 
   map.fitBounds(bounds);
-
-  var zoomChangeBoundsListener =
-  google.maps.event.addListener(map, 'bounds_changed', function(event) {
-    google.maps.event.removeListener(zoomChangeBoundsListener);
-    map.setZoom( Math.max( 12, map.getZoom() ) );
-  });
-
   map.panToBounds(bounds);
+
+  if(Object.keys(markers).length == 2)
+    $('.js-calculate-cost').removeClass('disabled');
+  else
+    $('.js-calculate-cost').addClass('disabled');
+}
+
+function calcRoute(start,end) {
+  var request = {
+    origin: start,
+    destination: end,
+    travelMode: 'DRIVING'
+  };
+
+  for(var key in markers){
+    markers[key].setMap(null);
+  }
+
+  directionsService.route(request, function(result, status) {
+    if (status == 'OK') {
+      directionsDisplay.setDirections(result);
+
+    }
+  });
+  map.setZoom(12);
 }
