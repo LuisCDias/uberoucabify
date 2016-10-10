8f861bb83f199173816af6a78fbf78b4dafd55f6
Luís Dias <lcdias127@gmail.com>
2016-10-10 21:43:23 +0100

    Social share buttons

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -36,6 +36,12 @@ document.addEventListener("turbolinks:load", function() {
     }
     calcRoute(fromInput.value,toInput.value)
   })
+
+  $('.fb-share, .twitter-share-button, .plus-share').click(function(e) {
+    e.preventDefault();
+    window.open($(this).attr('href'), 'fbShareWindow', 'height=450, width=550, top=' + ($(window).height() / 2 - 275) + ', left=' + ($(window).width() / 2 - 225) + ', toolbar=0, location=0, menubar=0, directories=0, scrollbars=0');
+    return false;
+  });
 });
 
 function togglePhoneImage(){
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def show
-    @live = false
+    @live = true
   end
 end
