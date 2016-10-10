25376aa66df015c1ab9b7e0420749fb2a8939b9f
Luís Dias <lcdias127@gmail.com>
2016-10-10 01:00:52 +0100

    Improving map markers

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,22 +1,21 @@
-var LOCATIONS = {
-  'porto': {lat: 41.165432, lng: -8.629065},
-  'lisbon': {lat: 38.728876, lng: -9.138234}
-}
+var LOCATIONS = { 'porto': {lat: 41.165432, lng: -8.629065}, 'lisbon': {lat: 38.728876, lng: -9.138234} };
 var CUSTOM_MAP_STYLES = [{"featureType":"water","elementType":"geometry","stylers":[{"color":"#e9e9e9"},{"lightness":17}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffffff"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#ffffff"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":16}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":21}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#dedede"},{"lightness":21}]},{"elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#ffffff"},{"lightness":16}]},{"elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#333333"},{"lightness":40}]},{"elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#f2f2f2"},{"lightness":19}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#fefefe"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#fefefe"},{"lightness":17},{"weight":1.2}]}];
+var markers = {};
 
 document.addEventListener("turbolinks:load", function() {
-  var phonePictureIntervalId = setInterval(function(){
-    $('.picture-container').toggleClass('showing-second-image');
-  }, 1500);
+  var phonePictureIntervalId = setInterval(togglePhoneImage, 1500);
+  setTimeout(function(){
+    clearInterval(phonePictureIntervalId);
+  }, 3000);
 
   $('.picture-container')
   .on('mouseover',function(){
-    console.log("over");
+    togglePhoneImage();
+    phonePictureIntervalId = setInterval(togglePhoneImage, 1500);
+  })
+  .on('mouseout',function(){
     clearInterval(phonePictureIntervalId);
-  })
-  .on('mousedown touchstart',function(){
-    $('.picture-container').toggleClass('showing-second-image');
-  })
+  });
 
   startGMaps();
   $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
@@ -35,6 +34,10 @@ document.addEventListener("turbolinks:load", function() {
   })
 });
 
+function togglePhoneImage(){
+  $('.picture-container').toggleClass('showing-second-image');
+}
+
 function startGMaps(){
   var defaultCity = "porto";
   async('maps.googleapis.com/maps/api/js?key=AIzaSyBSuC-_fpdtL7xCl1jWperw8d71Ce9jIeU&libraries=places', function() {
@@ -50,9 +53,16 @@ function showTab(city){
     styles: CUSTOM_MAP_STYLES,
     mapTypeId: google.maps.MapTypeId.ROADMAP,
     mapTypeControl: false,
-    streetViewControl: false
+    streetViewControl: false,
+    scrollwheel: false
   });
   loadPlacesSearch(map);
+  // var zoomChangeBoundsListener =
+  // google.maps.event.addListener(map, 'bounds_changed', function(event) {
+  //   alert("working");
+  //     google.maps.event.removeListener(zoomChangeBoundsListener);
+  //     map.setZoom( Math.min( 15, map.getZoom() ) );
+  // });
 }
 
 function loadPlacesSearch(map){
@@ -62,6 +72,8 @@ function loadPlacesSearch(map){
   var calculateButton = document.getElementById('calculate-cost-button');
   var fromSearchBox = new google.maps.places.SearchBox(fromInput);
   var toSearchBox = new google.maps.places.SearchBox(toInput);
+  var fromIconPath = fromInput.dataset.iconPath;
+  var toIconPath = toInput.dataset.iconPath;
   // TODO do something with toSearchBox
   map.controls[google.maps.ControlPosition.TOP_LEFT].push(fromInput);
   map.controls[google.maps.ControlPosition.LEFT_TOP].push(toInput);
@@ -70,54 +82,68 @@ function loadPlacesSearch(map){
   // Bias the SearchBox results towards current map's viewport.
   map.addListener('bounds_changed', function() {
     fromSearchBox.setBounds(map.getBounds());
+    toSearchBox.setBounds(map.getBounds());
   });
 
-  var markers = [];
+
   // Listen for the event fired when the user selects a prediction and retrieve
   // more details for that place.
   fromSearchBox.addListener('places_changed', function() {
     var places = fromSearchBox.getPlaces();
-
-    if (places.length == 0) {
-      return;
-    }
-
-    // Clear out the old markers.
-    markers.forEach(function(marker) {
-      marker.setMap(null);
-    });
-    markers = [];
+    if (places.length == 0) return;
 
     // For each place, get the icon, name and location.
-    var bounds = new google.maps.LatLngBounds();
-    places.forEach(function(place) {
-      if (!place.geometry) {
-        console.log("Returned place contains no geometry");
-        return;
-      }
-      var icon = {
-        url: place.icon,
-        size: new google.maps.Size(71, 71),
-        origin: new google.maps.Point(0, 0),
-        anchor: new google.maps.Point(17, 34),
-        scaledSize: new google.maps.Size(25, 25)
-      };
-
-      // Create a marker for each place.
-      markers.push(new google.maps.Marker({
-        map: map,
-        icon: icon,
-        title: place.name,
-        position: place.geometry.location
-      }));
-
-      if (place.geometry.viewport) {
-        // Only geocodes have viewport.
-        bounds.union(place.geometry.viewport);
-      } else {
-        bounds.extend(place.geometry.location);
-      }
-    });
-    map.fitBounds(bounds);
+    place = places[0]
+    setMarkerToPlace(place,map,'from');
   });
+  toSearchBox.addListener('places_changed', function() {
+    var places = toSearchBox.getPlaces();
+    if (places.length == 0) return;
+
+    // For each place, get the icon, name and location.
+    place = places[0]
+    setMarkerToPlace(place,map,'to');
+  });
+}
+
+function setMarkerToPlace(place,map,markerKey){
+  if (!place.geometry) {
+    console.log("Returned place contains no geometry");
+    return;
+  }
+  var icon = {
+    url: place.icon,
+    size: new google.maps.Size(71, 71),
+    origin: new google.maps.Point(0, 0),
+    anchor: new google.maps.Point(17, 34),
+    scaledSize: new google.maps.Size(25, 25)
+  };
+
+  // Clear out the old marker if exists.
+  if(markers[markerKey])
+    markers[markerKey].setMap(null);
+
+  // Create a marker for each place.
+  markers[markerKey] = new google.maps.Marker({
+    map: map,
+    icon: icon,
+    title: place.name,
+    position: place.geometry.location
+  });
+
+  var bounds = new google.maps.LatLngBounds();
+
+  for(var key in markers){
+    bounds.extend(markers[key].getPosition());
+  }
+
+  map.fitBounds(bounds);
+
+  var zoomChangeBoundsListener =
+  google.maps.event.addListener(map, 'bounds_changed', function(event) {
+    google.maps.event.removeListener(zoomChangeBoundsListener);
+    map.setZoom( Math.max( 12, map.getZoom() ) );
+  });
+
+  map.panToBounds(bounds);
 }
